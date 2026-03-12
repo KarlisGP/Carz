1e5f4ec50f4dbb36c3ddc90a7b1d9f17923e0257
Tomass <cerstomass@gmail.com>
2026-03-12 19:11:50 +0200

    uwu

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -4,42 +4,33 @@ public class PlayerController : MonoBehaviour
 {
     public float speed = 20.0f;
     public float turnSpeed = 40f;
+
     private float horizontalInput;
     private float forwardInput;
+
     public Camera mainCamera;
     public Camera hoodCamera;
 
+    public string inputID;
     public KeyCode switchKey;
 
-    // Start is called once before the first execution of Update after the MonoBehaviour is created
-    void Start()
-    {
-
-    }
-
-    // Update is called once per frame
     void Update()
     {
-// Axis setup
-        horizontalInput = Input.GetAxis("Horizontal");
-        forwardInput =
-            Input.GetAxis("Vertical");
-// Move the vehicle forward
+        // Multiplayer input
+        horizontalInput = Input.GetAxis("Horizontal" + inputID);
+        forwardInput = Input.GetAxis("Vertical" + inputID);
+
+        // Move forward
         transform.Translate(Vector3.forward * Time.deltaTime * speed * forwardInput);
-// Rotate the vehicle left and right
+
+        // Turn
         transform.Rotate(Vector3.up, turnSpeed * horizontalInput * Time.deltaTime);
+
+        // Camera switch
         if (Input.GetKeyDown(switchKey))
         {
             mainCamera.enabled = !mainCamera.enabled;
-            hoodCamera.enabled
-                = !hoodCamera.enabled;
-
-            if (Input.GetKeyDown(switchKey))
-            {
-                mainCamera.enabled = !mainCamera.enabled;
-                hoodCamera.enabled = !hoodCamera.enabled;
-            }
+            hoodCamera.enabled = !hoodCamera.enabled;
         }
     }
 }
-    
